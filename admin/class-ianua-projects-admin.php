e4991b6f127fa0a8c2314234448f1d51f7e69ea9
Jasper Frumau <jasper@imagewize.com>
2017-02-19 13:26:10 +0300

    added shortcode for cpt

--- admin/class-ianua-projects-admin.php
+++ admin/class-ianua-projects-admin.php
@@ -163,6 +163,35 @@ class Ianua_Projects_Admin {
 	}
 	//add_action( 'init', 'ianua_projects_cpt', 0 );
 
+	// create shortcode to list all clothes which come in blue
+	// add_shortcode( 'list-posts-basic', 'ianua_post_listing_shortcode' );
+	static function ianua_post_listing_shortcode( $atts ) {
+	    ob_start();
+	    $query = new WP_Query( array(
+	        'post_type' => 'ianua_projects',
+	        //'color' => 'blue',
+	        'posts_per_page' => -1,
+	        'order' => 'ASC',
+	        'orderby' => 'title',
+	    ) );
+	    if ( $query->have_posts() ) { ?>
+	        <ul class="clothes-listing">
+	            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+	            <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	            </li>
+	            <?php endwhile;
+	            wp_reset_postdata(); ?>
+	        </ul>
+	    <?php $myvariable = ob_get_clean();
+	    return $myvariable;
+	    };
+	}
+
+	function ianua_projects_register_shortcodes() { //https://developer.wordpress.org/reference/functions/add_shortcode/
+		add_shortcode( 'ianua-projects', array('Ianua_Projects_Admin', 'ianua_post_listing_shortcode' ));
+	}
+
 	/**
 	 * Register the stylesheets for the admin area.
 	 *
@@ -208,8 +237,6 @@ class Ianua_Projects_Admin {
 		wp_enqueue_script( $this->Ianua_Projects, plugin_dir_url( __FILE__ ) . 'js/ianua-projects-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
-
-
 
 
 
