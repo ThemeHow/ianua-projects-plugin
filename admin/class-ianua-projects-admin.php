fea3682f73dd12797ee69bc5f4da7682adecf63b
Jasper Frumau <jasper@imagewize.com>
2017-02-19 08:24:43 +0300

    hide cpt and tax from public

--- admin/class-ianua-projects-admin.php
+++ admin/class-ianua-projects-admin.php
@@ -151,9 +151,9 @@ class Ianua_Projects_Admin {
 			'show_in_admin_bar'     => true,
 			'show_in_nav_menus'     => true,
 			'can_export'            => true,
-			'has_archive'           => true,		
+			'has_archive'           => false, // no index or archive		
 			'exclude_from_search'   => false,
-			'publicly_queryable'    => true,
+			'publicly_queryable'    => false, // no single cpts
 			'rewrite'               => $rewrite,
 			'capability_type'       => 'page',
 			'show_in_rest'          => false,
