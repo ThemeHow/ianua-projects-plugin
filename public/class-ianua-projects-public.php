1878620b225261bc43516bd9184c97a38c8bcf7e
Jasper Frumau <jasper@imagewize.com>
2017-02-20 16:48:16 +0300

    fixed foreach reach to display all thumbnails in grid

--- public/class-ianua-projects-public.php
+++ public/class-ianua-projects-public.php
@@ -104,13 +104,13 @@ class Ianua_Projects_Public {
 	// add_shortcode( 'list-posts-basic', 'ianua_post_listing_shortcode' );
 	static function ianua_post_listing_shortcode( $atts ) {
 	    ob_start();
+
 	    $query = new WP_Query( array(
 	        'post_type' => 'ianua_projects',
 	        'posts_per_page' => -1,
 	        'order' => 'ASC',
 	        'orderby' => 'title',
-	    ) );
-	    if ( $query->have_posts() ) { ?>
+	    ) ); ?>
 	        <div id="content" class="project-list"> 
 
 			   	<div class="row project-intro">
@@ -134,41 +134,35 @@ class Ianua_Projects_Public {
 							<li><a href="#" data-filter=".iw-webdesign">Webdesign</a></li>
 							<li><a href="#" data-filter=".iw-branding">Branding</a></li>
 							<li><a href="#" data-filter=".iw-seo">SEO</a></li>
-						</ul>
-		     			
+						</ul>    			
 		     		</div>
-
 				    <div id="portfolio-wrapper" class="iw-projects bgrid-third tab-bgrid-half stack">
-						        <?php while ( $query->have_posts() ) : $query->the_post();global $post;?>
-			        
-			         	<div class="bgrid folio-item <?php $posttags = get_the_tags();
-													if ($posttags) {
-													  foreach($posttags as $tag) {
-													    echo $tag->name . ' '; 
-													  }
-													};?>">
-			               <div class="item-wrap">
-			                  <a href="<?php the_permalink(); ?> ">
+						<?php if ( $query->have_posts() ) { ?>
+						<?php while ( $query->have_posts() ) : $query->the_post();global $post;?>
+			         	<div class="bgrid folio-item <?php $posttags = get_the_tags();if ($posttags) { foreach($posttags as $tag) { echo $tag->name . ' '; }};?> ">
+			                <div class="item-wrap">
+			                    <a href="<?php the_permalink(); ?> ">
 				                 <?php
 				                 if ( has_post_thumbnail() ) {
 				                  echo get_the_post_thumbnail( $post->ID, 'ianua-projects'); //add_image_size('projects', 287, 295);
 				                  } ?>
 
 								<!-- <img src="images/portfolio/puremedia.jpg" alt="Puremedia"> -->
-			                     <div class="overlay"></div>                       
-			                     <div class="portfolio-item-meta">
-			     					      <h5><?php the_title(); ?></h5>
-			                        <p>Wordpress</p>
-			     					   </div> 
-			                     <div class="link-icon"><i class="fa fa-plus"></i></div>
-			                  </a>
-			               </div> <!--/item-wrap -->
-			        	</div> <!-- /folio-item -->
+				                    <div class="overlay"></div>                       
+				                    <div class="portfolio-item-meta">
+				     					<h5><?php the_title(); ?></h5>
+				                        <p>Wordpress</p>
+					     			</div> 
+				                    <div class="link-icon"><i class="fa fa-plus"></i></div>
+			                    </a>
+			                </div> <!--/item-wrap -->
+			            </div> <!-- /folio-item -->
+			            <?php endwhile; ?>
 				    </div> <!--/portfolio-wrapper -->
-			</div> <!-- /row-items -->
-		</div> <!-- /project-list -->
-			            <?php endwhile;
-			            wp_reset_postdata(); ?>
+				</div> <!-- /row-items -->
+			</div> <!-- /project-list -->
+			    
+			        <?php wp_reset_postdata(); ?>
 			    <?php $myvariable = ob_get_clean();
 			    return $myvariable;
 	    };
