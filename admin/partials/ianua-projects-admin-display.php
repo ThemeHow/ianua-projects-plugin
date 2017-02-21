946a0b479b0a08c9f8a368e2a4dd113424076389
Jasper Frumau <jasper@imagewize.com>
2017-02-21 16:16:06 +0300

    added initial display code

--- admin/partials/ianua-projects-admin-display.php
+++ admin/partials/ianua-projects-admin-display.php
@@ -14,3 +14,14 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
+<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+<form method="post" action="options.php"><?php
+
+settings_fields( $this->plugin_name . '-options' );
+
+do_settings_sections( $this->plugin_name );
+
+submit_button( 'Save Settings' );
+
+?></form>
